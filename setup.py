01927200a1782ba88a45387048db7e253655af41
Billy Shambrook <billyshambrook@Billys-Air.home>
2016-03-28 20:07:23 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='aiofirebase',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     description='Asyncio Firebase client library',
     author='Billy Shambrook',
